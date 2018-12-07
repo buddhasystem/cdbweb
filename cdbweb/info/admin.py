cf114493978506055fa0a88de9edfbe5fd667a66
Maxim Potekhin <potekhin@bnl.gov>
2018-12-06 21:54:51 -0500

    full assortment of tables added to the admin

--- cdbweb/info/admin.py
+++ cdbweb/info/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import AppMessage
+from .models import AppMessage, Basf2Module, GlobalTag, GlobalTagType, Payload, PayloadContent, PayloadIov, PayloadIovRpt, PayloadStatus
 
 ############
 class AppMessageAdmin(admin.ModelAdmin):
@@ -7,4 +7,115 @@ class AppMessageAdmin(admin.ModelAdmin):
     empty_value_display = '-empty-'
     
 admin.site.register(AppMessage, AppMessageAdmin)
+
+############
+class Basf2ModuleAdmin(admin.ModelAdmin):
+    list_display = ('basf2_module_id', 'name', 'next_revision', 'description', 'dtm_ins','dtm_mod','modified_by')
+    empty_value_display = '-empty-'
+    
+admin.site.register(Basf2Module, Basf2ModuleAdmin)
+
+############
+class GlobalTagAdmin(admin.ModelAdmin):
+    list_display = ('global_tag_id',
+                    'name',
+                    'is_default',
+                    'description',
+                    'global_tag_status',
+                    'global_tag_type',
+                    'dtm_ins',
+                    'dtm_mod',
+                    'modified_by')
+    empty_value_display = '-empty-'
+    
+admin.site.register(GlobalTag, GlobalTagAdmin)
+
+############
+class GlobalTagTypeAdmin(admin.ModelAdmin):
+    list_display = ('global_tag_type_id',
+                    'name',
+                    'description',
+                    'dtm_ins',
+                    'dtm_mod')
+
+    empty_value_display = '-empty-'
+    
+admin.site.register(GlobalTagType, GlobalTagTypeAdmin)
+
+############
+class PayloadAdmin(admin.ModelAdmin):
+    list_display = ('payload_id',
+                    'basf2_module',
+                    'revision',
+                    'description',
+                    'is_default',
+                    'base_url',
+                    'checksum',
+                    'payload_status',
+                    'deleted',
+                    'dtm_ins',
+                    'dtm_mod',
+                    'modified_by')
+
+    empty_value_display = '-empty-'
+    
+admin.site.register(Payload, PayloadAdmin)
+
+############
+class PayloadContentAdmin(admin.ModelAdmin):
+    list_display = ('payload_content_id',
+                    'payload',
+                    'content')
+
+    empty_value_display = '-empty-'
+    
+admin.site.register(PayloadContent, PayloadContentAdmin)
+
+############
+class PayloadIovAdmin(admin.ModelAdmin):
+    list_display = ('payload_iov_id',
+                    'global_tag_payload',
+                    'exp_start',
+                    'run_start',
+                    'exp_end',
+                    'run_end',
+                    'dtm_ins',
+                    'dtm_mod',
+                    'modified_by')
+
+    empty_value_display = '-empty-'
+    
+admin.site.register(PayloadIov, PayloadIovAdmin)
+
+############
+class PayloadIovRptAdmin(admin.ModelAdmin):
+    list_display = ('payload_iov_rpt_id',
+                    'global_tag_payload_id',
+                    'dtm_ins',
+                    'dtm_mod',
+                    'global_tag_id',
+                    'gt_name',
+                    'b2m_name',
+                    'exp_start',
+                    'exp_end',
+                    'run_start',
+                    'run_end',
+                    'payload_id',
+                    'payload_iov_id')
+
+    empty_value_display = '-empty-'
+    
+admin.site.register(PayloadIovRpt, PayloadIovRptAdmin)
+
+############
+class PayloadStatusAdmin(admin.ModelAdmin):
+    list_display = ('payload_status_id',
+                    'name',
+                    'description',
+                    'dtm_ins',
+                    'dtm_mod')
+
+    empty_value_display = '-empty-'
+    
+admin.site.register(PayloadStatus, PayloadStatusAdmin)
 
