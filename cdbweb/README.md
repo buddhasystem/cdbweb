ec36d2ce6afbc17e5510beb2bc51950a6f3cb823
Maxim Potekhin <potekhin@bnl.gov>
2018-12-06 19:32:34 -0500

    Edited models, added README

---
+++ cdbweb/README.md
@@ -0,0 +1,1 @@
+The SQL here is kept for reference purposes to facilitate development, it's not used directly.
--- cdbweb/models.py
+++ cdbweb/models.py
@@ -1,10 +1,6 @@
-# This is an auto-generated Django model module.
-# You'll have to do the following manually to clean this up:
-#   * Rearrange models' order
-#   * Make sure each model has one field with primary_key=True
-#   * Make sure each ForeignKey has `on_delete` set to the desired behavior.
-#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
-# Feel free to rename the models, but don't rename db_table values or field names.
+# commented out the models/tables which appear to exist in the dump
+# but not in the initialization SQL script
+
 from django.db import models
 
 
@@ -63,18 +59,6 @@ class GlobalTagPayload(models.Model):
         unique_together = (('global_tag', 'payload'),)
 
 
-class GlobalTagPayload20180302Save(models.Model):
-    global_tag_payload_id = models.IntegerField(blank=True, null=True)
-    global_tag_id = models.IntegerField(blank=True, null=True)
-    payload_id = models.IntegerField(blank=True, null=True)
-    dtm_ins = models.DateTimeField(blank=True, null=True)
-    dtm_mod = models.DateTimeField(blank=True, null=True)
-
-    class Meta:
-        managed = False
-        db_table = 'global_tag_payload_20180302_save'
-
-
 class GlobalTagStatus(models.Model):
     global_tag_status_id = models.AutoField(primary_key=True)
     name = models.TextField(unique=True)
@@ -86,7 +70,6 @@ class GlobalTagStatus(models.Model):
         managed = False
         db_table = 'global_tag_status'
 
-
 class GlobalTagType(models.Model):
     global_tag_type_id = models.AutoField(primary_key=True)
     name = models.TextField(unique=True, blank=True, null=True)
@@ -97,31 +80,6 @@ class GlobalTagType(models.Model):
     class Meta:
         managed = False
         db_table = 'global_tag_type'
-
-
-class GtpSave(models.Model):
-    global_tag_payload_id = models.IntegerField(blank=True, null=True)
-    global_tag_id = models.IntegerField(blank=True, null=True)
-    payload_id = models.IntegerField(blank=True, null=True)
-    dtm_ins = models.DateTimeField(blank=True, null=True)
-    dtm_mod = models.DateTimeField(blank=True, null=True)
-
-    class Meta:
-        managed = False
-        db_table = 'gtp_save'
-
-
-class GtpSave20170317(models.Model):
-    global_tag_payload_id = models.IntegerField(blank=True, null=True)
-    global_tag_id = models.IntegerField(blank=True, null=True)
-    payload_id = models.IntegerField(blank=True, null=True)
-    dtm_ins = models.DateTimeField(blank=True, null=True)
-    dtm_mod = models.DateTimeField(blank=True, null=True)
-
-    class Meta:
-        managed = False
-        db_table = 'gtp_save_20170317'
-
 
 class Payload(models.Model):
     payload_id = models.AutoField(primary_key=True)
@@ -142,26 +100,6 @@ class Payload(models.Model):
         managed = False
         db_table = 'payload'
         unique_together = (('payload_id', 'revision'), ('basf2_module', 'revision'),)
-
-
-class Payload20180302Save(models.Model):
-    payload_id = models.IntegerField(blank=True, null=True)
-    basf2_module_id = models.IntegerField(blank=True, null=True)
-    revision = models.IntegerField(blank=True, null=True)
-    description = models.TextField(blank=True, null=True)
-    is_default = models.BooleanField(blank=True, null=True)
-    base_url = models.TextField(blank=True, null=True)
-    payload_url = models.TextField(blank=True, null=True)
-    checksum = models.TextField(blank=True, null=True)
-    payload_status_id = models.IntegerField(blank=True, null=True)
-    deleted = models.BooleanField(blank=True, null=True)
-    dtm_ins = models.DateTimeField(blank=True, null=True)
-    dtm_mod = models.DateTimeField(blank=True, null=True)
-    modified_by = models.TextField(blank=True, null=True)
-
-    class Meta:
-        managed = False
-        db_table = 'payload_20180302_save'
 
 
 class PayloadContent(models.Model):
@@ -191,22 +129,6 @@ class PayloadIov(models.Model):
         unique_together = (('global_tag_payload', 'exp_start', 'run_start', 'exp_end', 'run_end'),)
 
 
-class PayloadIov20180302Save(models.Model):
-    payload_iov_id = models.IntegerField(blank=True, null=True)
-    global_tag_payload_id = models.IntegerField(blank=True, null=True)
-    exp_start = models.IntegerField(blank=True, null=True)
-    run_start = models.IntegerField(blank=True, null=True)
-    exp_end = models.IntegerField(blank=True, null=True)
-    run_end = models.IntegerField(blank=True, null=True)
-    dtm_ins = models.DateTimeField(blank=True, null=True)
-    dtm_mod = models.DateTimeField(blank=True, null=True)
-    modified_by = models.TextField(blank=True, null=True)
-
-    class Meta:
-        managed = False
-        db_table = 'payload_iov_20180302_save'
-
-
 class PayloadIovRpt(models.Model):
     payload_iov_rpt_id = models.AutoField(primary_key=True)
     global_tag_payload_id = models.IntegerField(blank=True, null=True)
@@ -226,27 +148,6 @@ class PayloadIovRpt(models.Model):
         managed = False
         db_table = 'payload_iov_rpt'
 
-
-class PayloadIovRpt20180302Save(models.Model):
-    payload_iov_rpt_id = models.BigIntegerField(blank=True, null=True)
-    global_tag_payload_id = models.IntegerField(blank=True, null=True)
-    dtm_ins = models.DateTimeField(blank=True, null=True)
-    dtm_mod = models.DateTimeField(blank=True, null=True)
-    global_tag_id = models.IntegerField(blank=True, null=True)
-    gt_name = models.TextField(blank=True, null=True)
-    b2m_name = models.TextField(blank=True, null=True)
-    exp_start = models.IntegerField(blank=True, null=True)
-    exp_end = models.IntegerField(blank=True, null=True)
-    run_start = models.IntegerField(blank=True, null=True)
-    run_end = models.IntegerField(blank=True, null=True)
-    payload_id = models.IntegerField(blank=True, null=True)
-    payload_iov_id = models.IntegerField(blank=True, null=True)
-
-    class Meta:
-        managed = False
-        db_table = 'payload_iov_rpt_20180302_save'
-
-
 class PayloadStatus(models.Model):
     payload_status_id = models.AutoField(primary_key=True)
     name = models.TextField(unique=True)
@@ -257,3 +158,97 @@ class PayloadStatus(models.Model):
     class Meta:
         managed = False
         db_table = 'payload_status'
+
+
+# class GlobalTagPayload20180302Save(models.Model):
+#     global_tag_payload_id = models.IntegerField(blank=True, null=True)
+#     global_tag_id = models.IntegerField(blank=True, null=True)
+#     payload_id = models.IntegerField(blank=True, null=True)
+#     dtm_ins = models.DateTimeField(blank=True, null=True)
+#     dtm_mod = models.DateTimeField(blank=True, null=True)
+
+#     class Meta:
+#         managed = False
+#         db_table = 'global_tag_payload_20180302_save'
+
+
+# class GtpSave(models.Model):
+#     global_tag_payload_id = models.IntegerField(blank=True, null=True)
+#     global_tag_id = models.IntegerField(blank=True, null=True)
+#     payload_id = models.IntegerField(blank=True, null=True)
+#     dtm_ins = models.DateTimeField(blank=True, null=True)
+#     dtm_mod = models.DateTimeField(blank=True, null=True)
+
+#     class Meta:
+#         managed = False
+#         db_table = 'gtp_save'
+
+
+# class GtpSave20170317(models.Model):
+#     global_tag_payload_id = models.IntegerField(blank=True, null=True)
+#     global_tag_id = models.IntegerField(blank=True, null=True)
+#     payload_id = models.IntegerField(blank=True, null=True)
+#     dtm_ins = models.DateTimeField(blank=True, null=True)
+#     dtm_mod = models.DateTimeField(blank=True, null=True)
+
+#     class Meta:
+#         managed = False
+#         db_table = 'gtp_save_20170317'
+
+
+# class Payload20180302Save(models.Model):
+#     payload_id = models.IntegerField(blank=True, null=True)
+#     basf2_module_id = models.IntegerField(blank=True, null=True)
+#     revision = models.IntegerField(blank=True, null=True)
+#     description = models.TextField(blank=True, null=True)
+#     is_default = models.BooleanField(blank=True, null=True)
+#     base_url = models.TextField(blank=True, null=True)
+#     payload_url = models.TextField(blank=True, null=True)
+#     checksum = models.TextField(blank=True, null=True)
+#     payload_status_id = models.IntegerField(blank=True, null=True)
+#     deleted = models.BooleanField(blank=True, null=True)
+#     dtm_ins = models.DateTimeField(blank=True, null=True)
+#     dtm_mod = models.DateTimeField(blank=True, null=True)
+#     modified_by = models.TextField(blank=True, null=True)
+
+#     class Meta:
+#         managed = False
+#         db_table = 'payload_20180302_save'
+
+
+# class PayloadIov20180302Save(models.Model):
+#     payload_iov_id = models.IntegerField(blank=True, null=True)
+#     global_tag_payload_id = models.IntegerField(blank=True, null=True)
+#     exp_start = models.IntegerField(blank=True, null=True)
+#     run_start = models.IntegerField(blank=True, null=True)
+#     exp_end = models.IntegerField(blank=True, null=True)
+#     run_end = models.IntegerField(blank=True, null=True)
+#     dtm_ins = models.DateTimeField(blank=True, null=True)
+#     dtm_mod = models.DateTimeField(blank=True, null=True)
+#     modified_by = models.TextField(blank=True, null=True)
+
+#     class Meta:
+#         managed = False
+#         db_table = 'payload_iov_20180302_save'
+
+
+# class PayloadIovRpt20180302Save(models.Model):
+#     payload_iov_rpt_id = models.BigIntegerField(blank=True, null=True)
+#     global_tag_payload_id = models.IntegerField(blank=True, null=True)
+#     dtm_ins = models.DateTimeField(blank=True, null=True)
+#     dtm_mod = models.DateTimeField(blank=True, null=True)
+#     global_tag_id = models.IntegerField(blank=True, null=True)
+#     gt_name = models.TextField(blank=True, null=True)
+#     b2m_name = models.TextField(blank=True, null=True)
+#     exp_start = models.IntegerField(blank=True, null=True)
+#     exp_end = models.IntegerField(blank=True, null=True)
+#     run_start = models.IntegerField(blank=True, null=True)
+#     run_end = models.IntegerField(blank=True, null=True)
+#     payload_id = models.IntegerField(blank=True, null=True)
+#     payload_iov_id = models.IntegerField(blank=True, null=True)
+
+#     class Meta:
+#         managed = False
+#         db_table = 'payload_iov_rpt_20180302_save'
+
+
