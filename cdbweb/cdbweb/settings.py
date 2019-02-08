2e7fc5cf1727f381cea8a348fc2712a46f4d37f0
Maxim Potekhin <potekhin@bnl.gov>
2019-02-08 13:30:51 +0900

    remove extraneous file, rename the auth db

--- cdbweb/cdbweb/settings.py
+++ cdbweb/cdbweb/settings.py
@@ -92,7 +92,7 @@ DATABASES = {
         'HOST': '',
         'PORT': '',
     },
-    'auth': {
+    'auth_db': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
