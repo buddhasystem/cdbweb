38ae321a29237c438cab6da253de23423919cdd5
Maxim Potekhin <potekhin@bnl.gov>
2019-03-11 18:56:00 -0400

    switching back to routers and sqlite for django auth stuff

--- cdbweb/cdbweb/settings.py
+++ cdbweb/cdbweb/settings.py
@@ -97,14 +97,14 @@ DATABASES = {
         'HOST': '',
         'PORT': '',
     },
-#    'auth_db': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#    }
+    'auth_db': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
 
 
-# DATABASE_ROUTERS = ['routers.AuthRouter.AuthRouter.AuthRouter','routers.PrimaryRouter.PrimaryRouter.PrimaryRouter',]
+DATABASE_ROUTERS = ['routers.AuthRouter.AuthRouter.AuthRouter','routers.PrimaryRouter.PrimaryRouter.PrimaryRouter',]
 
 
 # DATABASES = {
